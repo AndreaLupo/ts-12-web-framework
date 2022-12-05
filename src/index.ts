e7218f5b47d7e2aa5fecf4724f14c787b081e669
Wolf Developer <lupo.andrea.93@gmail.com>
2022-12-05 18:33:14 +0100

    Event handling

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,15 @@ const user = new User({ name: 'mnef', age: 12 });
 user.set({ name: 'newName' });
 
 console.log(user.get('name'), user.get('age'));
+
+user.on('change', () => {
+  console.log('change1');
+});
+user.on('change', () => {
+  console.log('change2');
+});
+user.on('save', () => {
+  console.log('Save was triggered!');
+});
+
+user.trigger('save');
--- src/models/User.ts
+++ src/models/User.ts
@@ -2,7 +2,13 @@ interface UserProps {
   name?: string;
   age?: number;
 }
+
+type Callback = () => void;
+
 export class User {
+  // array of strings as key
+  events: { [key: string]: Callback[] } = {};
+
   constructor(private data: UserProps) { }
 
   get(propName: string): (string | number) {
@@ -12,5 +18,23 @@ export class User {
   set(update: UserProps): void {
     // copy props from the second object into the first object
     Object.assign(this.data, update);
+  }
+
+  on(eventName: string, callback: Callback): void {
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  }
+
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+
+    if (!handlers || handlers.length === 0) {
+      return;
+    }
+
+    handlers.forEach(callback => {
+      callback();
+    });
   }
 }
