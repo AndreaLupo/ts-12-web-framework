8cbbd518b4e05d7f0755d6e4d2ca25879944c5f2
Wolf Developer <lupo.andrea.93@gmail.com>
2022-12-05 19:21:09 +0100

    Add fetch and save to user

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,20 @@
-import axios from 'axios';
+/* import axios from 'axios';
 
 axios.post('http://localhost:3000/users', {
   name: 'myName',
   age: 20
-}); 
+});  */
+
+import { User } from "./models/User";
+
+const user = new User({ id: 1 });
+
+// user.fetch();
+
+// update user
+user.set({ name: 'NEW NAME', age: 40 });
+user.save();
+
+// new user
+const baby = new User({ name: 'Carletto', age: 2 });
+baby.save();
